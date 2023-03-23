a914c4a7dcca0c5635cd94df02fa58d3ca39236e
Esekyi <sskert10@gmail.com>
2023-03-23 01:30:03 +0000

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - check main.
+ * @i: integer i
+ * @j: integer j
+ * Description: take two int as arguments and return results.
+ * Return: an integer results.
+ */
+
+int add(int i, int j)
+{
+	int result = 0;
+
+	result = i + j;
+	return (result);
+}
