398c0942a4a42db34e48c130efdfbebe74fdf4cc
Esekyi <sskert10@gmail.com>
2023-04-05 05:26:23 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strspn - check code
+ * @s: pointer containing string
+ * @accept: character to be found
+ * Description: Locates a character in a string
+ * Return: a pointer to the first character found
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	char *d_strt = accept;
+	int itrt = 0, raise;
+
+	while (*s)
+	{
+		raise = 0;
+		while (*accept)
+		{
+			if (*s == *accept)
+			{
+				itrt++;
+				raise = 1;
+				break;
+			}
+			accept++;
+		}
+		s++;
+		accept = d_start;
+		if (raise == 0)
+			break;
+	}
+	return (itrt);
+}
