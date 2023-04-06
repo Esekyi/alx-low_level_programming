93dff81d276f8038692a5afa7350fb07c864dfb2
Esekyi <sskert10@gmail.com>
2023-04-06 02:25:41 +0000

    _puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check code.
+ * @s: sreing input
+ * Description: function that prints a string, followed by a new line.
+ * Return: Nothing.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,6 @@
+# RECURSION
+__GENERAL__
+* `What is recursion` 
+* `How to implement recursion`
+* `In what situations you should implement recursion`
+* `In what situations you shouldn/'t implement recursion.`
