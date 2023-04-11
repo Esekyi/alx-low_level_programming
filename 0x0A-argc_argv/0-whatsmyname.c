2eeb20dd439ea36a56efe9c57a722ddf8dca03f7
Esekyi <sskert10@gmail.com>
2023-04-11 02:31:25 +0000

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  check code
+ * @argv: array of string
+ * @argc: number of arguments passed on command line
+ * Description: a program that prints its name, followed by a new line
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
