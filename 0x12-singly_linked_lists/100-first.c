cad057b99b0a2df14e0a40886b6592348852d60a
Esekyi <sskert10@gmail.com>
2023-04-28 04:37:42 +0000

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+void _constructor(void) __attribute__((constructor));
+
+/**
+ * _constructor - a constructor function
+ *
+ * Return: Nothing.
+ */
+
+void __constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
