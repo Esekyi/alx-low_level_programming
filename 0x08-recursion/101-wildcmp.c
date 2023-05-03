24609fb5f0fcb54236520fd3b3d106b6ed8108c9
Esekyi <sskert10@gmail.com>
2023-05-03 10:05:31 +0000

    checker failed, fixes

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,16 +7,24 @@
  * Description: compares two string
  * Return: 1 if the strings are identical.
  */
-
 int wildcmp(char *s1, char *s2)
 {
 	if (!*s1 && !*s2)
 		return (1);
+	if (*s2 == '*')
+	{
+		/* skip over multiple consecutive '*' in s2 */
+		while (*(s2 + 1) == '*')
+			s2++;
+		if (!*s1 && *(s2 + 1))
+			return (0);
+
+		if (wildcmp(s1, s2 + 1) || (*s1 && wildcmp(s1 + 1, s2)))
+			return (1);
+
+		return (0);
+	}
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-		return (1);
-	if (*s2 == '*' && *(s1 + 1) && *s2)
-		return (0);
 	return (0);
 }
