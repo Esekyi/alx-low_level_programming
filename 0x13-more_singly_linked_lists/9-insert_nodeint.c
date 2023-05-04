2740a1f11115de771a9f443669b2f3d7496231d7
Esekyi <sskert10@gmail.com>
2023-05-04 02:48:01 +0000

    tried to fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,44 +11,40 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newNode, *cur_node;
-	unsigned int i;
+	listint_t *newNode, *tempList = *head;
+	unsigned int i = 1;
 
-	if (!head)
+	if (head == NULL)
 		return (NULL);
 
+	newNode = malloc(sizeof(listint_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+
 	if (idx == 0)
-		return (add_nodeint(head, n));
-
-	cur_node = *head;
-	for (i = 0; cur_node && i < idx - 1; i++)
-		cur_node = cur_node->next;
-
-	if (i == idx - 1 || !cur_node)
 	{
-		newNode = malloc(sizeof(listint_t));
-		if (!newNode)
-			return (NULL);
-		newNode->n = n;
-		newNode->next = NULL;
-
-		if (!cur_node)
-			cur_node = *head;
-
-		if (i == idx - 1)
-		{
-			newNode->next = cur_node->next;
-			cur_node->next = newNode;
-		}
-		else
-		{
-			while (cur_node->next)
-				cur_node = cur_node->next;
-			cur_node->next = newNode;
-		}
+		newNode->next = *head;
+		*head = newNode;
 
 		return (newNode);
 	}
 
-	return (NULL);
+	while (tempList != NULL && i < idx - 1)
+	{
+		tempList = tempList->next;
+		i++;
+	}
+
+	if (tempList == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->next = tempList->next;
+	tempList->next = newNode;
+	return (newNode);
 }
