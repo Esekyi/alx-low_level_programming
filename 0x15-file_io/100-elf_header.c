e78b7acbda05a333d49648696605a2d2331d69fe
Esekyi <sskert10@gmail.com>
2023-05-09 05:47:13 +0000

    fix

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-		return 98;
+		return (98);
 	}
 
 	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
