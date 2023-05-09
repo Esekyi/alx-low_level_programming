72f4f845e43637f12f8a323b8c6faac607634d92
Esekyi <sskert10@gmail.com>
2023-05-09 05:58:43 +0000

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,2 +1,54 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * main - Entry point, check code.
+ * @argc: argument count, number passed
+ * @argv: pointers to agument array.
+ * Description: Main Entry point to the program
+ * Return: 1 if success and -1 on fail
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	_cp_file(argv[1], argv[2]);
+	exit(0);
+}
+
+/**
+ * _cp_file - check code
+ * @src: source file
+ * des: destination 
+ * Description: nothing here
+ * Return: 1 or -1
+ */
+
+void _cp_file(const char *src, const char *des)
+{
+	int _read, fd, cfd;
+	char buffer[1024];
+
+	cfd = open(src, O_RDONLY);
+	if (!src || cfd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		exit(98);
+	}
+
+	fd = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	
+	while ((_read = read(cfd, buffer, 1024)) > 0)
+	{
+		if (write(fd, buffer, _read) != _read || fd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", des);
+			exit(99);
+		}
+	}
+}
