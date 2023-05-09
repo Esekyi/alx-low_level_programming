3b93a7267b4360f0a77fad090d95179451bd1dca
Esekyi <sskert10@gmail.com>
2023-05-09 04:57:47 +0000

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		ssize_t write_bytes = write(fd, text_content, _strlen(text_content));
+		write(fd, text_content, _strlen(text_content));
 	}
 
 	close(fd);
